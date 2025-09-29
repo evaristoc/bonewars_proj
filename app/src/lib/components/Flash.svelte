7ca760155ae51fd79512efefff67f12a2678151b
unknown <varikvi@yahoo.com>
2025-09-29 14:38:52 +0200

    epic/landingpage : organizing structure; adding Flash component

---
+++ app/src/lib/components/Flash.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { flashInnerText, src, alt, innerTextrotation, imgrotation } = $props();
+</script>
+
+<div class="flash">
+	<p class="flash-content" style:rotate={innerTextrotation}>{flashInnerText}</p>
+	<img class="flash-img" {src} {alt} style:rotate={imgrotation} />
+</div>
+
+<style>
+</style>
